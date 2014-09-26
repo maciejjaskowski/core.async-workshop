6c24b075fa8615490c862cef74b1164c1d96c905
Maciej Jaskowski <maciej.jaskowski@gmail.com>
2014-09-26 11:25:54 +0200

    Done.

--- src/core/async_workshop.clj
+++ src/core/async_workshop.clj
@@ -1,10 +1,16 @@
-
 ; If everything is OK, you should be able to copy-paste the following lines without error.
 
-(require '[clojure.core.async :refer [chan >! <! >!! <!! onto-chan] :as a])
+; Documentation: http://clojure.github.io/core.async/
+
+; Official walkthrough: https://github.com/clojure/core.async/blob/master/examples/walkthrough.clj
+
+; Rich Hickey: http://clojure.com/blog/2013/06/28/clojure-core-async-channels.html
+
+(require '[clojure.core.async :refer [chan >! <! >!! <!! alts!! close! go onto-chan] :as a])
+(require '[clojure.core.async.lab :as al])
 (require '[clojure.test :refer :all])
 
-
+; == Channels basics
 ; channel is like a fifo queue with a buffer
 (def buffer-size 1)
 (def ch1 (chan buffer-size))
@@ -15,20 +21,154 @@
 (def read (<!! ch1))
 (is (= read 5))
 
-; Exercise 1: try in REPL: (<!! ch) what happens?
+; ** Exercise 1a:
+;   try in REPL: (<!! ch) what happens?
+; ** Exercise 1b:
+;   you can create an unbuferred channel by invoking (chan)
+;   what is going to happen if you send there a lot of values?
 
+
+
+; == Transformations of channels
 ; channels are fun because you can use map etc.
 (def ch-int (chan 10))
 (>!! ch-int 40)
 (def ch-str (a/map str [ch-int]))
 (is (= (<!! ch-str) "40"))
 
-;; Exersice 2: write a
-;(def ch-ints (chan 10))
-;(a/onto-chan ch-ints (range 1 10))
-;(def ch-even ch-ints)
-;(a/filter<)
-;(is (= (<!! ch-even) 2))
-;(is (= (<!! ch-even) 4))
-;(is (= (<!! ch-even) 6))
 
+; Sometimes a transducer better suits your needs:
+(def ch-str (chan 1 (map str)))
+(>!! ch-str 40)
+(is (= (<!! ch-str) "40"))
+
+; ** Exercise 2:
+;   Use filter transducer to create channel ch-even into which you serve integers but read only even numbers
+
+(def ch-even)
+
+(>!! ch-even 1)
+(>!! ch-even 2)
+(close! ch-even)
+(is (= (<!! ch-even) 2))
+(is (= (<!! ch-even) nil))
+
+
+
+; thanks to transducers you can even reduce the whole channel into a single value
+; ** Exercise 2a:
+;   use a/reduce function in place of ???
+
+(def ch-int (chan 3))
+(def ch-sum ???)
+(>!! ch-int 1)
+(>!! ch-int 1)
+(>!! ch-int 1)
+(close! ch-int)                                             ; Note that we close! the ch-inc before reading from ch-sum !
+(is (= (<!! ch-sum) 3))
+
+
+; == Channels as means of inter thread communication
+; We can safely communicate two threads using channels:
+
+(def c1 (chan))
+(a/thread (while true
+            (let [v (<!! c1)]
+              (println "Read" v))))
+(a/thread (do
+            (>!! c1 "hi")))
+
+; And we can use alts!! to choose from which channel to read
+(let [c1 (chan)
+      c2 (chan)]
+  (a/thread (while true
+              (let [[v ch] (alts!! [c1 c2])]
+                (println "Read" v "from" ch))))
+  (a/thread (do
+              (>!! c1 "hi")
+              (>!! c2 "there"))))
+
+; ** Exercise 3:
+; Using alts!! change the implementation below so that the thread
+; stops if a something gets pushed into poison-pills channel
+(let [c1 (chan)
+      poison-pills (chan)]
+  (a/thread (while true
+              (let [v (>!! c1 1)]
+                (<! (timeout 100))
+                (println "Read" v))))
+  (>!! c1 "hi"))
+
+
+
+; == Go Blocks
+; The real power comes with Go blocks
+
+(def c1 (chan))
+(go (while true
+      (let [v (<! c1)]
+        (println "Read" v))))
+(go
+  (>! c1 "hi"))
+
+; Compare this to the version from
+; "Channels as means of inter thread communication" section
+; The only diff is we substituted "thread"s by "go"s!
+;
+; Yet now the <! and >! operations do not block the thread executing a go block.
+; Instead the execution is suspended and the thread moved to other tasks.
+
+
+; == Testing Go Blocks
+; One of the excellent parts of go blocks is that testing it is very easy
+; because all you need is to put things on the input wire and read result from the output wire:
+
+(def c1 (chan))
+(def c2 (chan))
+
+(go
+  (loop [a 0]
+    (if-let [v (<! c1)]
+      (let [sum (+ v a)]
+        (>! c2 sum)
+        (recur sum)))))
+
+(al/spool [1 1 1 3] c1)
+(is (= (<!! c2) 1))
+(is (= (<!! c2) 2))
+(is (= (<!! c2) 3))
+(is (= (<!! c2) 6))
+
+
+; == Integration with callbacks
+
+(defn async-operation [x callback]
+  (a/thread
+    (Thread/sleep 100)
+    (callback (* x x))))
+
+(defn <async-operation [x]
+  (let [c (chan 1)]
+    (async-operation x
+                     (fn [result]
+                       (do
+                         (a/put! c result)
+                         (close! c))))
+    c))
+
+(def chOut (chan))
+(go
+  (let [async-op-result (<! (<async-operation 5))]
+    (>! chOut async-op-result)))
+
+(is (= (<!! chOut) 25))
+
+
+; one can also write a general version transforming functions with callbacks into channel-functions:
+(defn <<< [f & args]
+  (let [c (chan)]
+    (apply f (concat args [(fn [x]
+                             (if (nil? x)
+                               (close! c)
+                               (a/put! c x)))]))
+    c))
---
+++ src/core/crawler.clj
@@ -0,0 +1,62 @@
+(ns core.crawler
+  (:require [clojure.core.async :refer [chan >! <! >!! <!! alts!! close! go onto-chan] :as a])
+  (:require [clojure.test :refer :all]))
+
+;Main Exercise:
+;Using Go blocks and Channels create a crawler that will asynchronously crawl an api
+;For simplicity our Api is just an interface and a dummy remote-api implementation is provided.
+;I recommend solving this exercise in a couple of steps.
+;
+;Step I:
+; Integrate this callback based api so that you can test it using channels.
+; Fix tests.
+;
+;Step II:
+;You should be able to send one request to the requestsCh (e.g. 1).
+;As a result a single body from Api should be registered in the answersCh receive port.
+;Make sure that closing requestsCh causes the answersCh to receive the answer and closes answersCh, too!
+;Write tests!
+;
+;Step III:
+;Each body from Api might carry zero or more links. Combine the channels in such a way that
+;the Crawler crawls the whole Api.
+;Write tests!
+;
+;Step IV:
+;The whole crawling thing might take looong. Add a timeout capability to the Crawler.
+;After a specified amount of time no more requests to Api shall be sent; all channels get closed; the answer so far is
+;available in answersCh
+;
+;Step V:
+;Some of the links are of more interest to us then others. Since crawling can time out, we would like to get them first.
+;Use java.util.concurrent.PriorityQueue or clojure.data.priority-map
+;Make sure you do not block a go-block.
+;Write tests with an example comparator!
+;
+;Step VI:
+;Since the FiberApi suspends the Fiber, only one Api request is being processed concurrently.
+;Change your Crawler in such a way that up to N requests can be processed concurrently.
+
+
+
+(defn remote-api [size]
+  (fn [link callback]
+    (a/thread
+      (if (> link (/ size 2))
+        (do
+          (Thread/sleep 50)
+          (callback {:content (str "I am " link) :links []}))
+        (do
+          (Thread/sleep 50)
+          (callback {:content (str "I am " link)
+                     :links   [(* 2 link) (+ 1 (* 2 link))]}))))))
+
+#_(is (= ((remote-api 5) 1)
+       {:content "I am 1" :links [2 3]}))
+#_(is (= ((remote-api 5) 2)
+       {:content "I am 2" :links [4 5]}))
+#_(is (= ((remote-api 5) 3)
+       {:content "I am 3" :links []}))
+
+(def requestsCh (chan 1))
+(def answersCh (chan))
